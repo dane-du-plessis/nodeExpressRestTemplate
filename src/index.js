6880226b845e6d84ab56743e1a77ae37f2706d4e
Dane du Plessis <daneasc@gmail.com>
2020-05-23 15:33:38 +0200

    added some REST

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
-app.get('/', (req,res) => {
-res.send('Getting this?');
 
+app.get('/', (req, res) => {
+  res.send('Getting this?');
 })
+
+app.post('/', (req,res) => {
+  return res.send('Received POST HTTP');
+})
+
+app.put('/', (req,res) => {
+  return res.send('Recieved PUT HTTP');
+})
+
+app.delete('/', (req,res) => {
+  return res.send('Recieved DELETE HTTP');
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
